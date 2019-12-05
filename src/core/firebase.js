853015035230cecf5dda3f5ba42fd114f7775952
Parth Koshta <parth.k@theghack.in>
2019-12-06 01:33:03 +0530

    firebase update

--- src/core/firebase.js
+++ src/core/firebase.js
@@ -1,14 +1,11 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCV9rjHxJb8RpvJb1yugDKlTW_HzVhP-dA",
-  authDomain: "assignment-f19f4.firebaseapp.com",
-  databaseURL: "https://assignment-f19f4.firebaseio.com",
-  projectId: "assignment-f19f4",
-  storageBucket: "assignment-f19f4.appspot.com",
-  messagingSenderId: "872761569152",
-  appId: "1:872761569152:web:ae8fce962e80f51afe5b65",
-  measurementId: "G-YQX5DHMT3D"
+  apiKey: "AIzaSyB96TsUW0Z73H4SniIn_dYsM9BOteSqDBY",
+  authDomain: "users-de333.firebaseapp.com",
+  databaseURL: "https://users-de333.firebaseio.com",
+  projectId: "users-de333",
+  storageBucket: "users-de333.appspot.com"
 };
 
 firebase.initializeApp(firebaseConfig);

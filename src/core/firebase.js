0e715e86814ea94d5bc1853fca7715a91f8c6ff0
Parth Koshta <parth.k@theghack.in>
2019-12-06 13:09:01 +0530

    init

--- src/core/firebase.js
+++ src/core/firebase.js
@@ -5,8 +5,11 @@ const firebaseConfig = {
   authDomain: "users-de333.firebaseapp.com",
   databaseURL: "https://users-de333.firebaseio.com",
   projectId: "users-de333",
-  storageBucket: "users-de333.appspot.com"
+  storageBucket: "users-de333.appspot.com",
+  messagingSenderId: "286398250820",
+  appId: "1:286398250820:web:914040441abf33ad942111"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 export default firebase;
